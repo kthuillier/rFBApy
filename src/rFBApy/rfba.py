72d2295aaa02123a2b4a06602fb51bdf6a832c85
kthuilli <kerian.thuillier@gmail.com>
2026-05-29 11:50:14 +0200

    Fix

--- src/rFBApy/rfba.py
+++ src/rFBApy/rfba.py
@@ -36,7 +36,7 @@ def update_regulatory_state(
     x: dict[str, int],
 ) -> dict[str, int]:
     mext_state = {m: 0 if w.get(m, 0) == 0 else 1 for m in mn.metabolites(True, False)}
-    r_state = {r: v.get(r, 0) == 0 for r in mn.reactions()}
+    r_state = {r: 0 if v.get(r, 0) == 0 else 1 for r in mn.reactions()}
     next_bn = bn(x | mext_state | r_state) | mext_state
     return next_bn
 
@@ -163,7 +163,7 @@ def estimate_state_duration(
             v,
             x,
         )
-    if x_plus_1 != x:
+    if x != x_plus_1:
         return 1
     return max(1, estimate_mn_state_duration_ts(mn, obj, biomass, tau, w, v))
 
